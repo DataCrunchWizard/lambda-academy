e8306114092f1aec21bcb7be4afd9ab7d20f1900
TranHoangNam960329 <14520565@gm.uit.edu.vn>
2019-01-06 20:25:01 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: 'LambdaDemy <Học thực tiễn - làm thực tế/>',
+    description: 'LambdaDemy - Học thực tiễn - làm thực tế',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
